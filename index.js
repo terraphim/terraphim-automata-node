257482ff81a42219a23418906f33eb3dc79c9473
Alexander Mikhalev <alex@metacortex.engineer>
2023-07-19 00:09:45 +0100

    Change org fixed index

--- index.js
+++ index.js
@@ -32,24 +32,24 @@ switch (platform) {
   case 'android':
     switch (arch) {
       case 'arm64':
-        localFileExisted = existsSync(join(__dirname, '@terraphim.ai/terraphim-automata-node.android-arm64.node'))
+        localFileExisted = existsSync(join(__dirname, 'terraphim-automata-node.android-arm64.node'))
         try {
           if (localFileExisted) {
-            nativeBinding = require('./@terraphim.ai/terraphim-automata-node.android-arm64.node')
+            nativeBinding = require('./terraphim-automata-node.android-arm64.node')
           } else {
-            nativeBinding = require('@terraphim.ai/terraphim-automata-node-android-arm64')
+            nativeBinding = require('@terraphim/terraphim-automata-node-android-arm64')
           }
         } catch (e) {
           loadError = e
         }
         break
       case 'arm':
-        localFileExisted = existsSync(join(__dirname, '@terraphim.ai/terraphim-automata-node.android-arm-eabi.node'))
+        localFileExisted = existsSync(join(__dirname, 'terraphim-automata-node.android-arm-eabi.node'))
         try {
           if (localFileExisted) {
-            nativeBinding = require('./@terraphim.ai/terraphim-automata-node.android-arm-eabi.node')
+            nativeBinding = require('./terraphim-automata-node.android-arm-eabi.node')
           } else {
-            nativeBinding = require('@terraphim.ai/terraphim-automata-node-android-arm-eabi')
+            nativeBinding = require('@terraphim/terraphim-automata-node-android-arm-eabi')
           }
         } catch (e) {
           loadError = e
@@ -62,36 +62,36 @@ switch (platform) {
   case 'win32':
     switch (arch) {
       case 'x64':
-        localFileExisted = existsSync(join(__dirname, '@terraphim.ai/terraphim-automata-node.win32-x64-msvc.node'))
+        localFileExisted = existsSync(join(__dirname, 'terraphim-automata-node.win32-x64-msvc.node'))
         try {
           if (localFileExisted) {
-            nativeBinding = require('./@terraphim.ai/terraphim-automata-node.win32-x64-msvc.node')
+            nativeBinding = require('./terraphim-automata-node.win32-x64-msvc.node')
           } else {
-            nativeBinding = require('@terraphim.ai/terraphim-automata-node-win32-x64-msvc')
+            nativeBinding = require('@terraphim/terraphim-automata-node-win32-x64-msvc')
           }
         } catch (e) {
           loadError = e
         }
         break
       case 'ia32':
-        localFileExisted = existsSync(join(__dirname, '@terraphim.ai/terraphim-automata-node.win32-ia32-msvc.node'))
+        localFileExisted = existsSync(join(__dirname, 'terraphim-automata-node.win32-ia32-msvc.node'))
         try {
           if (localFileExisted) {
-            nativeBinding = require('./@terraphim.ai/terraphim-automata-node.win32-ia32-msvc.node')
+            nativeBinding = require('./terraphim-automata-node.win32-ia32-msvc.node')
           } else {
-            nativeBinding = require('@terraphim.ai/terraphim-automata-node-win32-ia32-msvc')
+            nativeBinding = require('@terraphim/terraphim-automata-node-win32-ia32-msvc')
           }
         } catch (e) {
           loadError = e
         }
         break
       case 'arm64':
-        localFileExisted = existsSync(join(__dirname, '@terraphim.ai/terraphim-automata-node.win32-arm64-msvc.node'))
+        localFileExisted = existsSync(join(__dirname, 'terraphim-automata-node.win32-arm64-msvc.node'))
         try {
           if (localFileExisted) {
-            nativeBinding = require('./@terraphim.ai/terraphim-automata-node.win32-arm64-msvc.node')
+            nativeBinding = require('./terraphim-automata-node.win32-arm64-msvc.node')
           } else {
-            nativeBinding = require('@terraphim.ai/terraphim-automata-node-win32-arm64-msvc')
+            nativeBinding = require('@terraphim/terraphim-automata-node-win32-arm64-msvc')
           }
         } catch (e) {
           loadError = e
@@ -102,35 +102,35 @@ switch (platform) {
     }
     break
   case 'darwin':
-    localFileExisted = existsSync(join(__dirname, '@terraphim.ai/terraphim-automata-node.darwin-universal.node'))
+    localFileExisted = existsSync(join(__dirname, 'terraphim-automata-node.darwin-universal.node'))
     try {
       if (localFileExisted) {
-        nativeBinding = require('./@terraphim.ai/terraphim-automata-node.darwin-universal.node')
+        nativeBinding = require('./terraphim-automata-node.darwin-universal.node')
       } else {
-        nativeBinding = require('@terraphim.ai/terraphim-automata-node-darwin-universal')
+        nativeBinding = require('@terraphim/terraphim-automata-node-darwin-universal')
       }
       break
     } catch {}
     switch (arch) {
       case 'x64':
-        localFileExisted = existsSync(join(__dirname, '@terraphim.ai/terraphim-automata-node.darwin-x64.node'))
+        localFileExisted = existsSync(join(__dirname, 'terraphim-automata-node.darwin-x64.node'))
         try {
           if (localFileExisted) {
-            nativeBinding = require('./@terraphim.ai/terraphim-automata-node.darwin-x64.node')
+            nativeBinding = require('./terraphim-automata-node.darwin-x64.node')
           } else {
-            nativeBinding = require('@terraphim.ai/terraphim-automata-node-darwin-x64')
+            nativeBinding = require('@terraphim/terraphim-automata-node-darwin-x64')
           }
         } catch (e) {
           loadError = e
         }
         break
       case 'arm64':
-        localFileExisted = existsSync(join(__dirname, '@terraphim.ai/terraphim-automata-node.darwin-arm64.node'))
+        localFileExisted = existsSync(join(__dirname, 'terraphim-automata-node.darwin-arm64.node'))
         try {
           if (localFileExisted) {
-            nativeBinding = require('./@terraphim.ai/terraphim-automata-node.darwin-arm64.node')
+            nativeBinding = require('./terraphim-automata-node.darwin-arm64.node')
           } else {
-            nativeBinding = require('@terraphim.ai/terraphim-automata-node-darwin-arm64')
+            nativeBinding = require('@terraphim/terraphim-automata-node-darwin-arm64')
           }
         } catch (e) {
           loadError = e
@@ -144,12 +144,12 @@ switch (platform) {
     if (arch !== 'x64') {
       throw new Error(`Unsupported architecture on FreeBSD: ${arch}`)
     }
-    localFileExisted = existsSync(join(__dirname, '@terraphim.ai/terraphim-automata-node.freebsd-x64.node'))
+    localFileExisted = existsSync(join(__dirname, 'terraphim-automata-node.freebsd-x64.node'))
     try {
       if (localFileExisted) {
-        nativeBinding = require('./@terraphim.ai/terraphim-automata-node.freebsd-x64.node')
+        nativeBinding = require('./terraphim-automata-node.freebsd-x64.node')
       } else {
-        nativeBinding = require('@terraphim.ai/terraphim-automata-node-freebsd-x64')
+        nativeBinding = require('@terraphim/terraphim-automata-node-freebsd-x64')
       }
     } catch (e) {
       loadError = e
@@ -159,23 +159,23 @@ switch (platform) {
     switch (arch) {
       case 'x64':
         if (isMusl()) {
-          localFileExisted = existsSync(join(__dirname, '@terraphim.ai/terraphim-automata-node.linux-x64-musl.node'))
+          localFileExisted = existsSync(join(__dirname, 'terraphim-automata-node.linux-x64-musl.node'))
           try {
             if (localFileExisted) {
-              nativeBinding = require('./@terraphim.ai/terraphim-automata-node.linux-x64-musl.node')
+              nativeBinding = require('./terraphim-automata-node.linux-x64-musl.node')
             } else {
-              nativeBinding = require('@terraphim.ai/terraphim-automata-node-linux-x64-musl')
+              nativeBinding = require('@terraphim/terraphim-automata-node-linux-x64-musl')
             }
           } catch (e) {
             loadError = e
           }
         } else {
-          localFileExisted = existsSync(join(__dirname, '@terraphim.ai/terraphim-automata-node.linux-x64-gnu.node'))
+          localFileExisted = existsSync(join(__dirname, 'terraphim-automata-node.linux-x64-gnu.node'))
           try {
             if (localFileExisted) {
-              nativeBinding = require('./@terraphim.ai/terraphim-automata-node.linux-x64-gnu.node')
+              nativeBinding = require('./terraphim-automata-node.linux-x64-gnu.node')
             } else {
-              nativeBinding = require('@terraphim.ai/terraphim-automata-node-linux-x64-gnu')
+              nativeBinding = require('@terraphim/terraphim-automata-node-linux-x64-gnu')
             }
           } catch (e) {
             loadError = e
@@ -184,23 +184,23 @@ switch (platform) {
         break
       case 'arm64':
         if (isMusl()) {
-          localFileExisted = existsSync(join(__dirname, '@terraphim.ai/terraphim-automata-node.linux-arm64-musl.node'))
+          localFileExisted = existsSync(join(__dirname, 'terraphim-automata-node.linux-arm64-musl.node'))
           try {
             if (localFileExisted) {
-              nativeBinding = require('./@terraphim.ai/terraphim-automata-node.linux-arm64-musl.node')
+              nativeBinding = require('./terraphim-automata-node.linux-arm64-musl.node')
             } else {
-              nativeBinding = require('@terraphim.ai/terraphim-automata-node-linux-arm64-musl')
+              nativeBinding = require('@terraphim/terraphim-automata-node-linux-arm64-musl')
             }
           } catch (e) {
             loadError = e
           }
         } else {
-          localFileExisted = existsSync(join(__dirname, '@terraphim.ai/terraphim-automata-node.linux-arm64-gnu.node'))
+          localFileExisted = existsSync(join(__dirname, 'terraphim-automata-node.linux-arm64-gnu.node'))
           try {
             if (localFileExisted) {
-              nativeBinding = require('./@terraphim.ai/terraphim-automata-node.linux-arm64-gnu.node')
+              nativeBinding = require('./terraphim-automata-node.linux-arm64-gnu.node')
             } else {
-              nativeBinding = require('@terraphim.ai/terraphim-automata-node-linux-arm64-gnu')
+              nativeBinding = require('@terraphim/terraphim-automata-node-linux-arm64-gnu')
             }
           } catch (e) {
             loadError = e
@@ -208,12 +208,12 @@ switch (platform) {
         }
         break
       case 'arm':
-        localFileExisted = existsSync(join(__dirname, '@terraphim.ai/terraphim-automata-node.linux-arm-gnueabihf.node'))
+        localFileExisted = existsSync(join(__dirname, 'terraphim-automata-node.linux-arm-gnueabihf.node'))
         try {
           if (localFileExisted) {
-            nativeBinding = require('./@terraphim.ai/terraphim-automata-node.linux-arm-gnueabihf.node')
+            nativeBinding = require('./terraphim-automata-node.linux-arm-gnueabihf.node')
           } else {
-            nativeBinding = require('@terraphim.ai/terraphim-automata-node-linux-arm-gnueabihf')
+            nativeBinding = require('@terraphim/terraphim-automata-node-linux-arm-gnueabihf')
           }
         } catch (e) {
           loadError = e
